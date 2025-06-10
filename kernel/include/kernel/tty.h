83c642e7814f7ce7ad449eba153603c99f9161ee
Liam Turelid <turelid@chalmers.se>
2025-06-10 17:12:58 +0000

    Meaty done

---
+++ kernel/include/kernel/tty.h
@@ -0,0 +1,14 @@
+#ifndef _KERNEL_TTY_H
+#define _KERNEL_TTY_H
+
+#include <stddef.h>
+
+void terminal_initialize(void);
+void terminal_putchar(char c);
+void terminal_write(const char* data, size_t size);
+void terminal_writestring(const char* data);
+void print_arch_logo(void);
+void print_color_palette(void);
+
+#endif
+
